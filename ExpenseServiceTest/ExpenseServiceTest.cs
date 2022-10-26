326db74b7039460d562f0070729289c61e5b303c
Xu DU <XDu3@slb.com>
2022-10-26 12:00:01 +0800

    test(question1): create a test Should_return_expense_type_B_if_project_is_external_and_name_is_project_B

--- ExpenseServiceTest/ExpenseServiceTest.cs
+++ ExpenseServiceTest/ExpenseServiceTest.cs
@@ -33,8 +33,11 @@ namespace Expense.Service.Test
         public void Should_return_expense_type_B_if_project_is_external_and_name_is_project_B()
         {
             // given
+            var project = new Project(ProjectType.EXTERNAL, "Project B");
             // when
+            var result = ExpenseService.GetExpenseCodeByProjectTypeAndName(project);
             // then
+            Assert.Equal(ExpenseType.EXPENSE_TYPE_B, result);
         }
 
         [Fact]
